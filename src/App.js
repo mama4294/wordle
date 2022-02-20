cf9722dc22ca13a0e3e2efca9b2f05bcc6055a2c
Matthew Malone <mama4294@colorado.edu>
2022-02-20 14:59:45 -0500

    event listeners for key inputs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
-import {useState} from 'react';
+import {useEffect, useState, useRef} from 'react';
 
 function App() {
+  const answerWord = "genny"
+  let answerArr = answerWord.toUpperCase().split("")
+  const [stageInt, setStageInt] = useState(1);
+  const [inputs, setInputs] = useState([{stage: "1", values: [], status:[]}, {stage: "2", values: [], status:[]}, {stage: "3", values: [], status:[]}, {stage: "4", values: [], status:[]}, {stage: "5", values: [], status:[]}]);
   const [letters, setLetters]=useState([
     {value: "Q", status: "", row: "top"}, 
     {value: "W", status: "", row: "top"}, 
@@ -31,10 +35,93 @@ function App() {
     {value: "M", status: "", row: "bottom"}, 
   ])
 
+  function useEventLister(eventName, handler, element=window){
+    const savedHandler = useRef();
+
+    // Update saved handler if handler changes
+    useEffect(()=>{
+      savedHandler.current = handler
+    },[handler])
+
+    useEffect(()=>{
+      const isSupported = element && element.addEventListener
+      if(!isSupported) return;
+
+      const eventListener = event => savedHandler.current(event)
+      element.addEventListener(eventName, eventListener)
+
+      //Cleanup
+      return()=>{
+        element.removeEventListener(eventName,eventListener)
+      }
+    },[eventName,element])
+
+  }
+
+ const handleKeyPress = (event) =>{
+    handleLetter(event.key)
+  }
+
+  function isLetter(c) {
+    return /^[A-Z]$/i.test(c);
+  }
+
+  function changeLetterStatus(letter, status){
+    const letterIndex = letters.findIndex((obj) => obj.value == letter)
+    if(letters[letterIndex].status==="correct"|| status==='correct')
+      letters[letterIndex].status="correct"
+      
+    else if(status === "close"){
+      letters[letterIndex].status="close"
+
+    }else if(status === "incorrect"){
+      letters[letterIndex].status="incorrect"
+
+    }
+  }
+
+
+  const handleLetter = (letter) =>{
+    let currentInputs = [...inputs];
+    let currentStageInput = currentInputs[stageInt-1]
+    console.log(currentInputs)
+    if(letter == "Enter"){
+      console.log("submitting")
+      for (let i = 0; i<5; i++){  
+       if(currentStageInput.values[i]===answerArr[i]){
+        currentStageInput.status[i]="correct"
+        changeLetterStatus(currentStageInput.values[i],"correct")
+       }else if(answerArr.includes(currentStageInput.values[i])){
+        currentStageInput.status[i]="close"
+        changeLetterStatus(currentStageInput.values[i],"close")
+       }else{
+         currentStageInput.status[i]="incorrect"
+         changeLetterStatus(currentStageInput.values[i],"incorrect")
+       }
+     }
+
+      setStageInt(prevInt => prevInt + 1)
+    }
+    else if(letter == "Backspace"){
+      console.log("deleating")
+      currentStageInput.values.pop();
+    }
+    else if(isLetter(letter) && currentStageInput.values.length < 5){
+      console.log("adding")
+      currentStageInput.values.push(letter.toUpperCase());
+    }
+    currentInputs[stageInt-1] = currentStageInput;
+    setInputs(currentInputs)
+  }
+
+  useEventLister('keydown', handleKeyPress)
+
+
+
   return (
     <div className="App">
       <Header />
-      <Game letters={letters} setLetters={setLetters}/>
+      <Game letters={letters} setLetters={setLetters} stageInt={stageInt} setStageInt={setStageInt} inputs={inputs}/>
       <Keyboard letters={letters}/>
     </div>
   );
@@ -52,129 +139,62 @@ function Header() {
   );
 }
 
-function Game({letters, setLetters}) {
-  const [stageInt, setStageInt] = useState(1);
+function Game({inputs}) {
+
   return (
     <div className="mb-10">
-    <GridRow active={stageInt===1} setStageInt={setStageInt} stageInt={stageInt} setLetters={setLetters} letters={letters}/>
-    <GridRow active={stageInt===2} setStageInt={setStageInt} stageInt={stageInt} setLetters={setLetters} letters={letters}/>
-    <GridRow active={stageInt===3} setStageInt={setStageInt} stageInt={stageInt} setLetters={setLetters} letters={letters}/>
-    <GridRow active={stageInt===4} setStageInt={setStageInt} stageInt={stageInt} setLetters={setLetters} letters={letters}/>
-    <GridRow active={stageInt===5} setStageInt={setStageInt} stageInt={stageInt} setLetters={setLetters} letters={letters}/>
+    <GridRow inputs={inputs[0]}/>
+    <GridRow inputs={inputs[1]}/>
+    <GridRow inputs={inputs[2]}/>
+    <GridRow inputs={inputs[3]}/>
+    <GridRow inputs={inputs[4]}/>
     </div>
   );
 }
 
-function GridRow({active, setStageInt, stageInt, setLetters, letters}){
-
-  const [letter1, setLetter1] = useState("")
-  const [letter2, setLetter2] = useState("")
-  const [letter3, setLetter3] = useState("")
-  const [letter4, setLetter4] = useState("")
-  const [letter5, setLetter5] = useState("")
-  const [guess, setGuess] = useState('')
-  const [resultsArr, setResultArr]=useState(["unknown","unknown","unknown","unknown","unknown"])
-
-  const handleSubmit = (event) =>{
-    event.preventDefault();
-    const answerWord = "genny"
-    let answerArr = answerWord.toUpperCase().split("")
-    const guessArr = [letter1, letter2, letter3, letter4, letter5]
-
-    setGuess(guessArr.join(''))
-
-    //determine if the letters are correct
-    let tempArr = [];
-    let lettersCopy = [...letters];
-    let letterGuesses = [];
-    for (let i = 0; i<5; i++){  
-      
-       //create copy of letters
-      const objIndex = lettersCopy.findIndex(letter => letter.value == guessArr[i])
-      if(guessArr[i]===answerArr[i]){
-        tempArr[i]="correct"
-        lettersCopy[objIndex].status = "correct"
-      }else if(answerArr.includes(guessArr[i])){
-        tempArr[i]="close"
-        lettersCopy[objIndex].status = "close"
-      }else{
-        tempArr[i]="incorrect"
-        lettersCopy[objIndex].status = "incorrect"
-      }
-    }
-
-    setLetters(lettersCopy)
-    setResultArr(tempArr)
-    setStageInt(prevInt => prevInt + 1)
-
-    //Set focus to next grid
-    const forms = document.querySelectorAll("form");
-    console.log(forms[stageInt].elements[0])
-    forms[stageInt].elements[0].focus();
-  }
-
-  const changeTileFocus = (int,dir) =>{
-    if (dir === "inc" && int < 4){
-      return int + 1
-    }
-    else if (dir === "dec" && int >0 ){
-      return int - 1
-    }
-    else return int;
-  }
-
-  const handleKeyUp = (event) => {
-    const form = event.target.form;
-    const index = [...form].indexOf(event.target);
-    if (event.key.toLowerCase() === "backspace") {
-      form.elements[changeTileFocus(index, "dec")].focus();
-    }else if (event.key.toLowerCase() === "enter") {
-      handleSubmit(event)
-    }else if (event.key.toLowerCase() === "tab") {
-      //nothing
-    }else{
-      form.elements[changeTileFocus(index, "inc")].focus();
-    }
-  }
-
+function GridRow({inputs}){
+  const values = inputs.values;
+  const status = inputs.status;
 
   return(
   <div className="flex flex-inline justify-center">
-    <form onSubmit={handleSubmit}>
       {/* <p className="absolute top-0">Not in word list</p> */}
-      <GameTile handleKeyUp={handleKeyUp} value ={letter1} onChange={(e)=>setLetter1(e.target.value.toUpperCase())} status={resultsArr[0]} locked={!active}/>
-      <GameTile handleKeyUp={handleKeyUp} value ={letter2} onChange={(e)=>setLetter2(e.target.value.toUpperCase())} status={resultsArr[1]} locked={!active}/>
-      <GameTile handleKeyUp={handleKeyUp} value ={letter3} onChange={(e)=>setLetter3(e.target.value.toUpperCase())} status={resultsArr[2]} locked={!active}/>
-      <GameTile handleKeyUp={handleKeyUp} value ={letter4} onChange={(e)=>setLetter4(e.target.value.toUpperCase())} status={resultsArr[3]} locked={!active}/>
-      <GameTile handleKeyUp={handleKeyUp} value ={letter5} onChange={(e)=>setLetter5(e.target.value.toUpperCase())} status={resultsArr[4]} locked={!active}/>
-      {/* {active && <button type="sumbit">Enter</button>} */}
-  </form>
+      <GameTile value ={values[0]} status={status[0]}/>
+      <GameTile value ={values[1]} status={status[1]}/>
+      <GameTile value ={values[2]} status={status[2]}/>
+      <GameTile value ={values[3]} status={status[3]}/>
+      <GameTile value ={values[4]} status={status[4]}/>
 </div>
   )
 }
 
-function GameTile({handleKeyUp, onChange, value, status, locked}){
+function GameTile({value, status}){
+
+  let bgColor = ""
+  if(status==="correct"){
+    bgColor = "bg-green-400 text-white border-0"
+  }else if (status==="close"){
+    bgColor = "bg-yellow-400 text-white border-0"
+  }else if (status==="incorrect"){
+    bgColor = "bg-slate-500 text-white border-0"
+  }
 
   return(
-    <input
-    type="text"
-    className={`border h-16 w-16 md:h-24 md:w-24 border-slate-500 m-1 text-center text-4xl font-bold ${status==="correct" ? "bg-green-400" : "" } ${status==="incorrect" ? "bg-slate-400" : "" }  ${status==="close" ? "bg-yellow-400" : "" }`}
-    onKeyUp={handleKeyUp}
-    onChange={onChange}
-    value={value}
-    maxLength='1'
-    disabled={locked}
-    />  
+    <p
+    className={` border h-16 w-16 md:h-24 md:w-24 border-slate-300 m-1 text-4xl font-bold flex items-center justify-center ${bgColor}`}
+    >
+    {value}
+    </p>  
   )
 }
 
 function Keyboard({letters}){
   return (
-    <>
+    <div className="max-w-md m-auto">
       <Row letters={letters} row="top"/>
       <Row letters={letters} row="middle"/>
       <Row letters={letters} row="bottom"/>
-    </>
+    </div>
 
   )
 }
@@ -182,7 +202,7 @@ function Keyboard({letters}){
 function Row({row, letters}){
 return (
   <div className="w-full">
-    <div className="w-full flex justify-between p-2">
+    <div className="w-full flex justify-between p-1">
       {letters.map((letter) =>{
           return ( letter.row === row && <Key key={letter.value} value={letter.value} status={letter.status} />)
       })}
@@ -194,14 +214,14 @@ return (
 function Key({value, status}){
   let bgColor = "bg-slate-200"
   if(status==="correct"){
-    bgColor = "bg-green-400"
+    bgColor = "bg-green-400 text-white"
   }else if (status==="close"){
     bgColor = "bg-yellow-400"
   }else if (status==="incorrect"){
-    bgColor = "bg-slate-500"
+    bgColor = "bg-slate-500 text-white"
   }
   return (
-    <button className={`w-8 h-10 rounded ${bgColor}`}
+    <button className={`w-10 h-12 rounded p-2 font-bold ${bgColor}`}
     onClick={() =>{console.log(value)}}>
       {value}
     </button>
