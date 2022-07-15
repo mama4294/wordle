262b7201d9e2618157b37b1d61f9d1f48122a573
Matthew Malone <mama4294@colorado.edu>
2022-07-14 19:06:55 -0600

    Adds flipping animation on letter after submission

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,3 +1,5 @@
+import {motion} from "framer-motion"
+import { useState } from "react";
 
 const Game = ({ inputs, error }) => {
     return (
@@ -28,31 +30,56 @@ const Game = ({ inputs, error }) => {
     return (
       <div className="flex flex-inline justify-center">
         {/* <p className="absolute top-0">Not in word list</p> */}
-        <GameTile value={values[0]} status={status[0]} />
-        <GameTile value={values[1]} status={status[1]} />
-        <GameTile value={values[2]} status={status[2]} />
-        <GameTile value={values[3]} status={status[3]} />
-        <GameTile value={values[4]} status={status[4]} />
+        <GameTile i={0} value={values[0]} status={status[0]} />
+        <GameTile i={1} value={values[1]} status={status[1]} />
+        <GameTile i={2} value={values[2]} status={status[2]} />
+        <GameTile i={3} value={values[3]} status={status[3]} />
+        <GameTile i={4} value={values[4]} status={status[4]} />
       </div>
     );
   }
   
-  function GameTile({ value, status }) {
+  function GameTile({ value, status, i }) {
+    let [halfFlipped, setHalfFlipped] = useState(false);
     let bgColor = "";
-    if (status === "correct") {
-      bgColor = "bg-green-400 text-white border-0";
-    } else if (status === "close") {
-      bgColor = "bg-yellow-400 text-white border-0";
-    } else if (status === "incorrect") {
-      bgColor = "bg-slate-500 text-white border-0";
+    let rotation = 0
+
+    function onUpdate(latest) {
+      if(latest.rotateX > 90){
+        setHalfFlipped(true)
+      }else{
+        setHalfFlipped(false)
+      }
     }
-  
+
+    if(status === 'correct' || status === 'close' || status === 'incorrect'){
+      rotation = 180
+    }
+
+    if(halfFlipped){
+      if (status === "correct") {
+        bgColor = "bg-green-400 text-white border-0";
+      } else if (status === "close") {
+        bgColor = "bg-yellow-400 text-white border-0";
+      } else if (status === "incorrect") {
+        bgColor = "bg-slate-500 text-white border-0";
+      }
+    }
+
+
     return (
-      <p
-        className={` border h-12 w-12 md:h-16 md:w-16 border-slate-300 m-1 text-4xl font-bold flex items-center justify-center ${bgColor}`}
-      >
-        {value}
-      </p>
+      <motion.div
+        animate={{rotateX: rotation}}
+        transition={{type:'spring', duration: 2, bounce: 0.3, delay: i*0.2}}
+        onUpdate={onUpdate}>
+          <div className={halfFlipped? "rotate-x-180":""}>
+            <p
+            className={`border h-12 w-12 md:h-16 md:w-16 border-slate-300 m-1 text-4xl font-bold flex items-center justify-center ${bgColor}`}
+          >
+            {value}
+          </p>
+          </div>
+      </motion.div>
     );
   }
   
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".rotate-x-180": {
+          transform: "rotateX(180deg)",
+        },
+        ".content-hidden": {
+          "content-visibility": "hidden",
+        },
+        ".content-visible": {
+          "content-visibility": "visible",
+        },
+      });
+    }),
+  ],
 };
